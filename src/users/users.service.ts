6ff58c223877a43d7ac60ba28f555beb592c4ffb
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-12-17 16:44:53 +0900

    6.3 Verifying User part Two

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -47,7 +47,10 @@ export class UserService {
   }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
     // make a JWT and give it to the user
     try {
-      const user = await this.users.findOne({ email });
+      const user = await this.users.findOne(
+        { email },
+        { select: ['password'] },
+      );
       if (!user) {
         return {
           ok: false,
@@ -95,14 +98,21 @@ export class UserService {
   }
 
   async verifyEmail(code: string): Promise<boolean> {
-    const verification = await this.verifications.findOne(
-      { code },
-      { relations: ['user'] },
-    );
-    if (verification) {
-      verification.user.verified = true;
-      this.users.save(verification.user);
+    try {
+      const verification = await this.verifications.findOne(
+        { code },
+        { relations: ['user'] },
+      );
+      if (verification) {
+        verification.user.verified = true;
+        console.log(verification.user);
+        this.users.save(verification.user);
+        return true;
+      }
+      throw new Error();
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-    return false;
   }
 }
