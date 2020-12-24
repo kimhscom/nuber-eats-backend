3425634fb149c026868f2a090f49c0adc863100e
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-12-24 16:27:34 +0900

    11.2 Create Dish part Two

--- src/restaurants/entities/dish.entity.ts
+++ src/restaurants/entities/dish.entity.ts
@@ -11,8 +11,8 @@ class DishOption {
   name: string;
   @Field(type => [String], { nullable: true })
   choices?: string[];
-  @Field(type => Int)
-  extra: number;
+  @Field(type => Int, { nullable: true })
+  extra?: number;
 }
 
 @InputType('DishInputType', { isAbstract: true })
@@ -44,7 +44,7 @@ export class Dish extends CoreEntity {
   @ManyToOne(
     type => Restaurant,
     restaurant => restaurant.menu,
-    { onDelete: 'CASCADE' },
+    { onDelete: 'CASCADE', nullable: false },
   )
   restaurant: Restaurant;
 
