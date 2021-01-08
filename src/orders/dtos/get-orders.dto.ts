f28a1224968c816056857cef08cb3191a28397fa
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-01-08 22:21:15 +0900

    11.12 getOrders part One

---
+++ src/orders/dtos/get-orders.dto.ts
@@ -0,0 +1,15 @@
+import { InputType, Field, ObjectType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { OrderStatus, Order } from '../entities/order.entity';
+
+@InputType()
+export class GetOrdersInput {
+  @Field(type => OrderStatus, { nullable: true })
+  status?: OrderStatus;
+}
+
+@ObjectType()
+export class GetOrdersOutput extends CoreOutput {
+  @Field(type => [Order], { nullable: true })
+  orders?: Order[];
+}
