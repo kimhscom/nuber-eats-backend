bb304bb0b8c018def5d9c919ad0c136d06a57535
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-01-26 23:39:25 +0900

    13.6 Promoting Restaurants part One

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -172,6 +172,9 @@ export class RestaurantService {
         where: {
           category,
         },
+        order: {
+          isPromoted: 'DESC',
+        },
         take: 25,
         skip: (page - 1) * 25,
       });
@@ -195,6 +198,9 @@ export class RestaurantService {
       const [restaurants, totalResults] = await this.restaurants.findAndCount({
         skip: (page - 1) * 25,
         take: 25,
+        order: {
+          isPromoted: 'DESC',
+        },
       });
       return {
         ok: true,
