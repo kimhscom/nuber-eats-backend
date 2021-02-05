83b2d72268bb528ca40661b127ebd0dc7211eb00
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-06 02:09:42 +0900

    20.3 File Upload part Two

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -5,12 +5,41 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+const BUCKET_NAME = 'nubereatsclone123';
+const AWS_URL = `https://${BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com`;
 
 @Controller('uploads')
 export class UploadsController {
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+      region: 'ap-northeast-2',
+    });
+
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+
+      const url = `${AWS_URL}/${objectName}`;
+
+      return { url };
+    } catch (error) {
+      return null;
+    }
   }
 }
