263674d299b2e93c6b48c39a87e17bbe51205765
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-06 23:18:06 +0900

    Add my restaurant dto, service and resolver

---
+++ src/restaurants/dtos/my-restaurant.dto.ts
@@ -0,0 +1,12 @@
+import { InputType, PickType, ObjectType, Field } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Restaurant } from '../entities/restaurant.entity';
+
+@InputType()
+export class MyRestaurantInput extends PickType(Restaurant, ['id']) {}
+
+@ObjectType()
+export class MyRestaurantOutput extends CoreOutput {
+  @Field(type => Restaurant, { nullable: true })
+  restaurant?: Restaurant;
+}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -19,6 +19,10 @@ import {
   EditRestaurantInput,
   EditRestaurantOutput,
 } from './dtos/edit-restaurant.dto';
+import {
+  MyRestaurantInput,
+  MyRestaurantOutput,
+} from './dtos/my-restaurant.dto';
 import { MyRestaurantsOutput } from './dtos/my-restaurants.dto';
 import { RestaurantInput, RestaurantOutput } from './dtos/restaurant.dto';
 import { RestaurantsInput, RestaurantsOutput } from './dtos/restaurants.dto';
@@ -381,6 +385,27 @@ export class RestaurantService {
       return {
         ok: false,
         error: 'Could not find restaurants.',
+      };
+    }
+  }
+
+  async myRestaurant(
+    owner: User,
+    { id }: MyRestaurantInput,
+  ): Promise<MyRestaurantOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(
+        { owner, id },
+        { relations: ['menu', 'orders'] },
+      );
+      return {
+        ok: true,
+        restaurant,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not find restaurant',
       };
     }
   }
